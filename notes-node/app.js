7000556b11ce76e21c4b555a10645d313e95c1c5
bryan buitrago <bryanbuitrago86@gmail.com>
2017-02-13 16:55:17 -0500

    linking files inside app.js

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,16 @@
 console.log('Starting app.');
 //to load the modules functionality and call it down below use require()
-const fs = require('fs');
-const os = require('os');
+const fs = require('fs'); // require module fs
+const os = require('os'); //require module os
+const notes = require('./notes.js'); // file required from file path
 
-var user = os.userInfo(); // used userInfo() method
+var sum = notes.add(5,4);
+console.log(sum);
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var user = os.userInfo(); // used userInfo() method
 // console.log(user);
-
-fs.appendFile('greetings.txt', `hello ${user.username}!`); // used appendFile() method
-
 // fs.appendFile('greetings.txt', 'hello world!');
+// fs.appendFile('greetings.txt', `hello ${user.username}! You are ${notes.age}.`); // used appendFile() method
---
+++ notes-node/notes.js
@@ -0,0 +1,13 @@
+console.log('The file has been executed!!!');
+// console.log(module);
+
+// module.exports.age = 25;
+
+// module.exports.addNote = () => {
+//   console.log('addNote');
+//   return 'New note';
+// };
+
+module.exports.add = (a, b) => {
+  return a + b
+}
