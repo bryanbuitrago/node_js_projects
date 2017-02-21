e5dc5cec54a7888f580615a1dac2e44e19d2e4cb
bryan buitrago <bryanbuitrago86@gmail.com>
2017-02-21 14:39:11 -0500

    Up tp section 3 lecture 12

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,23 +1,34 @@
-console.log('Starting app.');
-//to load the modules functionality and call it down below use require()
-const fs = require('fs'); // require module fs
-const os = require('os'); //require module os
-const _ = require('lodash'); // third party module requiring
-const notes = require('./notes.js'); // file required from file path
+console.log('Starting App.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
+const fs = require('fs');
+const _ = require('loadsh');
 
-var filteredArray = _.uniq(['Bryan', 1, 'Bryan', 1, 2, 3, 3]);
-console.log(filteredArray);
+const notes = require('./notes.js');
 
-// var sum = notes.add(5,4);
-// console.log(sum);
 
-// var res = notes.addNote();
-// console.log(res);
 
-// var user = os.userInfo(); // used userInfo() method
-// console.log(user);
-// fs.appendFile('greetings.txt', 'hello world!');
-// fs.appendFile('greetings.txt', `hello ${user.username}! You are ${notes.age}.`); // used appendFile() method
+
+// ======== UP TO SECTION 3 LECTURE 11 ======
+// console.log('Starting app.');
+// //to load the modules functionality and call it down below use require()
+// const fs = require('fs'); // require module fs
+// const os = require('os'); //require module os
+// const _ = require('lodash'); // third party module requiring
+// const notes = require('./notes.js'); // file required from file path
+
+// // console.log(_.isString(true));
+// // console.log(_.isString('Andrew'));
+
+// var filteredArray = _.uniq(['Bryan', 1, 'Bryan', 1, 2, 3, 3]);
+// console.log(filteredArray);
+
+// // var sum = notes.add(5,4);
+// // console.log(sum);
+
+// // var res = notes.addNote();
+// // console.log(res);
+
+// // var user = os.userInfo(); // used userInfo() method
+// // console.log(user);
+// // fs.appendFile('greetings.txt', 'hello world!');
+// // fs.appendFile('greetings.txt', `hello ${user.username}! You are ${notes.age}.`); // used appendFile() method
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,13 +1,17 @@
-console.log('The file has been executed!!!');
-// console.log(module);
 
-// module.exports.age = 25;
 
-// module.exports.addNote = () => {
-//   console.log('addNote');
-//   return 'New note';
-// };
 
-module.exports.add = (a, b) => {
-  return a + b
-}
+// ======== UP TO SECTION 3 LECTURE 11 =========
+// console.log('The file has been executed!!!');
+// // console.log(module);
+
+// // module.exports.age = 25;
+
+// // module.exports.addNote = () => {
+// //   console.log('addNote');
+// //   return 'New note';
+// // };
+
+// module.exports.add = (a, b) => {
+//   return a + b
+// }
